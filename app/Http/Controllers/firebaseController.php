de2c0ee8f58aa4eac91535a4aa6c25a96fa4c5c7
habil <habiedgar111@gmail.com>
2022-11-25 11:13:11 +0700

    firebase connect

---
+++ app/Http/Controllers/firebaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// require_once __DIR__.'/../vendor/autoload.php';
+
+use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class firebaseController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+        // $serviceaccount = ServiceAccount::fromJsonFile(__DIR__.'/firebase_key.json');
+        $firebase = (new Factory)
+            ->withDatabaseUri('https://tugas-akhir-pemin-default-rtdb.asia-southeast1.firebasedatabase.app/');
+            // ->withServiceAccount(__DIR__.'/firebase_key.json');
+        $database = $firebase->createDatabase();
+        $ref = $database->getReference('Subjects');
+        $key = $ref->push()->getKey();
+        $ref->getChild($key)->set([
+            'SubjectName' => 'Laravel'
+        ]);
+        return $key;
+    }
+}
